161d05bda1dfdbc720ee7759c15f777f297ea5df
tayyaba sani <tayyaba.sani@yahoo.com>
2021-08-16 20:47:41 +0200

    coding tasks

--- src/com/codingtasks/Main.java
+++ src/com/codingtasks/Main.java
@@ -1,12 +1,20 @@
 package com.codingtasks;
 
+import java.util.Arrays;
+
 import static com.codingtasks.PascalTriangle.generate;
+import static com.codingtasks.RotateImage.rotate;
 import static com.codingtasks.ThreeSum.threeSum;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(threeSum(new int[]{-1,0,1,2,-1,-4}));
+        System.out.println(threeSum(new int[]{-1, 0, 1, 2, -1, -4}));
         System.out.println(generate(5));
+        int[][] matrix = {{5, 1, 9, 11}, {2, 4, 8, 10}, {13, 3, 6, 7}, {15, 14, 12, 16}};
+        rotate(matrix);
+        for (int[] row : matrix)
+            System.out.println(Arrays.toString(row));
     }
 }
+
---
+++ src/com/codingtasks/RotateImage.java
@@ -0,0 +1,41 @@
+package com.codingtasks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RotateImage {
+
+    //You are given an n x n 2D matrix representing an image, rotate the image by 90 degrees (clockwise).
+    //
+    //You have to rotate the image in-place, which means you have to modify the input 2D matrix directly. DO NOT allocate another 2D matrix and do the rotation.
+
+    //Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+    //Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+    public static void rotate(int[][] matrix) {
+        int i = 0;
+        while (i < matrix.length) {
+            List<Integer> list = new ArrayList<>();
+            for (int j = i + 1; j < matrix.length; j++) {
+                list.add(matrix[i][j]);
+            }
+            List<Integer> listTobeAdded = new ArrayList<>();
+            int indexCount = list.size() - 1;
+            for (int k = matrix.length - 1; k >= i; k--) {
+                listTobeAdded.add(matrix[k][i]);
+                if (indexCount >= 0) {
+                    matrix[k][i] = list.get(indexCount);
+                    indexCount--;
+                }
+            }
+            for (int rem = i - 1; rem >= 0; rem--) {
+                listTobeAdded.add(matrix[i][rem]);
+            }
+            for (int add = 0; add < matrix.length; add++) {
+                matrix[i][add] = listTobeAdded.get(add);
+            }
+            i++;
+        }
+//        return matrix;
+    }
+}
